7599e89ba90912433d01280bc754572371ed7c98
marcusstenbeck <marcus.stenbeck@gmail.com>
2016-01-30 11:25:06 +0100

    feat: update for use with babel 6

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,18 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
       { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' }
     ]
-  }
+  },
+
+  devtool: '#inline-source-map'
+
 };
