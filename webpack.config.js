c529b281a008d6ea4ceb97429a3384a93f00bafe
Nikolay Kim <ru.yaruson@gmail.com>
2017-01-15 20:31:49 +0300

    Resolve paths to fix forward slashes for Windows
    webpack-dev-server doesn't rebundle changes due to slashes issue
    described in
    https://webpack.github.io/docs/troubleshooting.html#windows-paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
-  context: __dirname + '/src',
+  context: path.join(__dirname, '/src'),
   entry: './entry.js',
 
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/build',
+    path: path.join(__dirname, '/build'),
     publicPath: 'http://localhost:8080/build/'
   },
 
